522a7f1a1d9b8857c04294c56d979a7c7e878063
n1ghtw1sh <116516842@qq.com>
2017-12-09 17:54:22 +0800

    1.0.3

--- app/electron.js
+++ app/electron.js
@@ -63,9 +63,9 @@ else {
   localUrl = 'file://' + path.join(__dirname, './dist/index.html');
 }
 
-// config.url = `http://61.175.100.14:5433/`;
+config.url = `http://61.175.100.14:5433/`;
 // config.url = 'http://localhost:3000/';
-config.url = 'http://220.189.207.18:3000/';
+// config.url = 'http://220.189.207.18:3000/';
 
 
 // 主程序初始化
